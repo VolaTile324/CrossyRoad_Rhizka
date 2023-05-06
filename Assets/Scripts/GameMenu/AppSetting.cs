a5e61b84e427d2475390e684b61588cdbd5c4b4b
Rhizka Subentar <rhizkandro@gmail.com>
2023-05-06 22:53:36 +0700

    resolution confirm dialog
    Player now must press 'Apply' once more to ensure they're happy with resolution setting.
    Meanwhile, blip sound (which can be heard on using UI buttons and countdowns) have it's volume slightly adjusted, now less louder.

--- Assets/Scripts/GameMenu/AppSetting.cs
+++ Assets/Scripts/GameMenu/AppSetting.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Events;
 
 public class AppSetting : MonoBehaviour
 {
@@ -15,6 +16,10 @@ public class AppSetting : MonoBehaviour
     [SerializeField] TMP_Text sfxVolText;
     [SerializeField] private TMP_Dropdown resolutionDropdown;
     [SerializeField] Toggle windowedModeToggle;
+    [SerializeField] GameObject confirmDialog;
+
+    private int currentResolutionIndex;
+    private int previousResolutionIndex;
 
     private void OnEnable()
     {
@@ -32,7 +37,6 @@ public class AppSetting : MonoBehaviour
     {
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
-        int currentResolutionIndex = 0;
         for (int i = 0; i < Screen.resolutions.Length; i++)
         {
             string option = Screen.resolutions[i].width + " x " + Screen.resolutions[i].height;
@@ -44,19 +48,59 @@ public class AppSetting : MonoBehaviour
             }
         }
         resolutionDropdown.AddOptions(options);
-        currentResolutionIndex = PlayerPrefs.GetInt("Resolution", 0);
+        // set the current resolution, if playerprefs not found, set to max value
+        currentResolutionIndex = PlayerPrefs.GetInt("Resolution", int.MaxValue);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+
+        // store previous resolution
+        previousResolutionIndex = currentResolutionIndex;
     }
 
+    // once palyer presses 'Apply Video Settings' button, this function will be called
     public void SetResolution()
     {
         Resolution resolution = Screen.resolutions[resolutionDropdown.value];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
         resolutionDropdown.RefreshShownValue();
+        CheckResolution();
+        confirmDialog.gameObject.SetActive(true);
+    }
 
+    // this function will check confirmation from player if the resolution is ok
+    // after 10 seconds of no confirmation, it will revert to previous resolution
+    // a dialog box will appear to ask for confirmation from successful OnSetResolution.Invoke()
+    // if player presses 'Yes', it will save the current resolution to playerprefs
+    public void CheckResolution()
+    {
+        StartCoroutine(ResConfirmation());
+    }
+
+    IEnumerator ResConfirmation()
+    {
+        yield return new WaitForSecondsRealtime(10);
+        if (currentResolutionIndex != resolutionDropdown.value)
+        {
+            RevertResolution();
+        }
+    }
+
+    public void SaveResolution()
+    {
         PlayerPrefs.SetInt("Resolution", resolutionDropdown.value);
         PlayerPrefs.Save();
+        // update previous res index
+        previousResolutionIndex = resolutionDropdown.value;
+        confirmDialog.gameObject.SetActive(false);
+    }
+
+    public void RevertResolution()
+    {
+        resolutionDropdown.value = previousResolutionIndex;
+        Resolution resolution = Screen.resolutions[resolutionDropdown.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        resolutionDropdown.RefreshShownValue();
+        confirmDialog.gameObject.SetActive(false);
     }
 
     public void SetWindowedMode(bool value)
